f8a02d2bf2ac76f39a2b91f42982088766291b9c
janaka <janakamwm@siu.edu>
2023-05-19 17:03:03 -0500

    Baud rate increased to 1Mbps to achieve faster communication (transmission tested, receive does not tested)

--- scr/main.c
+++ scr/main.c
@@ -69,7 +69,8 @@ int main(void)
     //pinMode(D13, OUTPUT);
     degug_init();
     DynAx18aInit();
-    SerialInit(115200,NONE,EIGHT,ONE);
+    //SerialInit(115200,NONE,EIGHT,ONE);
+    SerialInit(1000000,NONE,EIGHT,ONE);
     Timer0Init(time_update);
 
     sei(); // Enable global interrupts
