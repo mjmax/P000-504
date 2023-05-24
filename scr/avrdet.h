37efd61140cc7f1737fa1d1712fe12800f205c4d
janaka <janakamwm@siu.edu>
2023-05-24 13:59:03 -0500

    corrected the microcontroller switching statements

--- scr/avrdet.h
+++ scr/avrdet.h
@@ -3,16 +3,20 @@
 
 /*Config Microcontroller*/
 #define AVR_ATmega328P  0x01
-//#define AVR_ATmega2560 0x01
+//#define AVR_ATmega2560  0x01
 
 #ifdef AVR_ATmega328P
 #define F_CPU 16588800UL
 #define __AVR_ATmega328P__
+#define __AVR_ATmega328P__
 #endif
 
+//#ifdef AVR_ATmega2560
 #ifdef AVR_ATmega2560
 #define F_CPU 16588800UL
+#ifndef __AVR_ATmega2560__
 #define __AVR_ATmega2560__
+#endif
 #endif
 
 #define NULL ((void *)0)
